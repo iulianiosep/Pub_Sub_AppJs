64791e1fd195ec9b2773d5fa0ef0f8ce4e4e6613
iulianiosep <iosep.iulian@yahoo.com>
2016-06-09 23:35:14 +0300

    SBE: Update subscription for sending in local network !

--- Publisher/app/components/ng/services/connectToServer.js
+++ Publisher/app/components/ng/services/connectToServer.js
@@ -48,6 +48,193 @@ angular.module('PublisherApp')
                                 value : 64.668722
                             },
                             date : '2016.02.04'
+                        },
+                        {
+                            id: 'P4',
+                            company : 'Google',
+                            value : {
+                                value : 75.318735
+                            },
+                            variation : {
+                                value : 68.668722
+                            },
+                            date : '2016.03.01'
+                        },
+                        {
+                            id: 'P5',
+                            company : 'Zapp',
+                            value : {
+                                value : 68.318735
+                            },
+                            variation : {
+                                value : 64.668722
+                            },
+                            date : '2016.02.04'
+                        },
+                        {
+                            id: 'P6',
+                            company : 'Orange',
+                            value : {
+                                value : 38.654789
+                            },
+                            variation : {
+                                value : 64.668722
+                            },
+                            date : '2018.06.14'
+                        },
+                        {
+                            id: 'P7',
+                            company : 'Vodafone',
+                            value : {
+                                value : 78.963254
+                            },
+                            variation : {
+                                value : 82.963213
+                            },
+                            date : '2019.01.24'
+                        },
+                        {
+                            id: 'P8',
+                            company : 'Mercedes',
+                            value : {
+                                value : 98.212365
+                            },
+                            variation : {
+                                value : 25.552634
+                            },
+                            date : '2012.02.09'
+                        },
+                        {
+                            id: 'P9',
+                            company : 'Google',
+                            value : {
+                                value : 38.658565
+                            },
+                            variation : {
+                                value : 28.112233
+                            },
+                            date : '2017.01.01'
+                        },
+                        {
+                            id: 'P10',
+                            company : 'Zapp',
+                            value : {
+                                value : 38.659823
+                            },
+                            variation : {
+                                value : 85.632541
+                            },
+                            date : '2013.12.11'
+                        },
+                        {
+                            id: 'P11',
+                            company : 'Orange',
+                            value : {
+                                value : 28.212365
+                            },
+                            variation : {
+                                value : 65.552634
+                            },
+                            date : '2019.02.01'
+                        },
+                        {
+                            id: 'P12',
+                            company : 'Volkswagen',
+                            value : {
+                                value : 68.111658
+                            },
+                            variation : {
+                                value : 48.659321
+                            },
+                            date : '2016.01.09'
+                        },
+                        {
+                            id: 'P13',
+                            company : 'Microsoft',
+                            value : {
+                                value : 78.313695
+                            },
+                            variation : {
+                                value : 45.654712
+                            },
+                            date : '2013.07.11'
+                        },
+                        {
+                            id: 'P14',
+                            company : 'Zapp',
+                            value : {
+                                value : 96.369742
+                            },
+                            variation : {
+                                value : 45.963548
+                            },
+                            date : '2011.06.21'
+                        },
+                        {
+                            id: 'P15',
+                            company : 'Orange',
+                            value : {
+                                value : 14.292365
+                            },
+                            variation : {
+                                value : 96.532634
+                            },
+                            date : '2012.02.09'
+                        },
+                        {
+                            id: 'P16',
+                            company : 'Microsoft',
+                            value : {
+                                value : 48.963451
+                            },
+                            variation : {
+                                value : 45.654931
+                            },
+                            date : '2015.12.19'
+                        },
+                        {
+                            id: 'P17',
+                            company : 'Telekom',
+                            value : {
+                                value : 88.265415
+                            },
+                            variation : {
+                                value : 25.818165
+                            },
+                            date : '2012.11.14'
+                        },
+                        {
+                            id: 'P18',
+                            company : 'Mercedes',
+                            value : {
+                                value : 78.212461
+                            },
+                            variation : {
+                                value : 75.253631
+                            },
+                            date : '2019.12.01'
+                        },
+                        {
+                            id: 'P19',
+                            company : 'Mercedes',
+                            value : {
+                                value : 47.211165
+                            },
+                            variation : {
+                                value : 35.559534
+                            },
+                            date : '2012.02.09'
+                        },
+                        {
+                            id: 'P20',
+                            company : 'Cosmote',
+                            value : {
+                                value : 98.000368
+                            },
+                            variation : {
+                                value : 65.653277
+                            },
+                            date : '2016.06.10'
                         }
                     ];
                 };
--- Subscriber/app/components/ng/services/subscriptionService.js
+++ Subscriber/app/components/ng/services/subscriptionService.js
@@ -45,6 +45,150 @@ angular.module('SubscriberApp')
                 value : 64.668722
               },
               date : '2016.02.04'
+            },
+            {
+              id: 'S4',
+              
+              variation : {
+                symbol: '>=',
+                value : 92.654852
+              },
+              date : '2015.03.02'
+            },
+            {
+              id: 'S5',
+              
+              variation : {
+                symbol: '>',
+                value : 45.156745
+              },
+              date : '2017.04.02'
+            },
+            {
+              id: 'S6',
+              
+              variation : {
+                symbol: '<',
+                value : 15.722668
+              },
+              date : '2016.02.04'
+            },
+            {
+              id: 'S7',
+              
+              variation : {
+                symbol: '<',
+                value : 37.357418
+              },
+              date : '2016.05.14'
+            },
+            {
+              id: 'S8',
+              
+              variation : {
+                symbol: '<',
+                value : 27.324568
+              },
+              date : '2016.01.26'
+            },
+            {
+              id: 'S9',
+              
+              variation : {
+                symbol: '<',
+                value : 37.357418
+              },
+              date : '2016.05.14'
+            },
+            {
+              id: 'S10',
+              
+              variation : {
+                symbol: '>',
+                value : 75.326985
+              },
+              date : '2019.10.20'
+            },
+            {
+              id: 'S11',
+              
+              variation : {
+                symbol: '<',
+                value : 48.987456
+              },
+              date : '2012.03.02'
+            },
+            {
+              id: 'S12',
+              
+              variation : {
+                symbol: '=',
+                value : 67.112233
+              },
+              date : '2018.12.20'
+            },
+            {
+              id: 'S13',
+              
+              variation : {
+                symbol: '=',
+                value : 80.326188
+              },
+              date : '2016.03.30'
+            },
+            {
+              id: 'S15',
+              
+              variation : {
+                symbol: '>',
+                value : 72.652971
+              },
+              date : '2019.07.21'
+            },
+            {
+              id: 'S16',
+              
+              variation : {
+                symbol: '=',
+                value : 63.963258
+              },
+              date : '2017.05.04'
+            },
+            {
+              id: 'S17',
+              
+              variation : {
+                symbol: '<',
+                value : 57.357419
+              },
+              date : '2016.12.17'
+            },
+            {
+              id: 'S18',
+              
+              variation : {
+                symbol: '<',
+                value : 17.951478
+              },
+              date : '2015.07.14'
+            },
+            {
+              id: 'S19',
+              
+              variation : {
+                symbol: '=',
+                value : 37.357418
+              },
+              date : '2016.05.14'
+            },
+            {
+              id: 'S20',
+              
+              variation : {
+                symbol: '<',
+                value : 87.357418
+              },
+              date : '2011.07.10'
             }
           ];
         };
